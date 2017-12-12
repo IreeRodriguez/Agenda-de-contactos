c676ddab0d4a957e3b590595d70ddd3778e8cbd2
IreeRodriguez <ire208@gmail.com>
2017-12-12 18:25:44 -0300

    probando modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
-	
-
-
+	$('.add').click(function(){
+		 $('.modalForm').show();
+	});
+	$('.close').click(function(){
+		 $('.modalForm').hide();
+	});
 
 });
+
+function saveContact(){
+	var name = $('#name').val();
+	var phone = $('#phone').val();
+
+	$('.contacts').append("<div class='panel panel-info'>" +
+
+	"<div class='panel-heading'>" + name +
+	"</div>" +
+	"<div class='panel-body'>" + phone +
+	"</div>" + "</div>")
+
+};
