cc7f07bed21381d368ebf87daa310b3df5112074
IreeRodriguez <ire208@gmail.com>
2017-12-12 21:43:39 -0300

    modal funcionando

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,17 +6,23 @@ $(document).ready(function(){
 		 $('.modalForm').hide();
 	});
 
+	$('.btnSubmit').click(saveContact);
+
+	function saveContact(){
+		var name = $('#name').val();
+		var phone = $('#phone').val();
+
+		$('.contacts').append("<div class='panel panel-info'>" +
+
+		"<div class='panel-heading'>" + name +
+		"</div>" +
+		"<div class='panel-body'>" + phone +
+		"</div>" + "</div>")
+
+		$('.modalForm').hide();
+		$('#name').val('');
+		$('#phone').val('');
+
+
+	};
 });
-
-function saveContact(){
-	var name = $('#name').val();
-	var phone = $('#phone').val();
-
-	$('.contacts').append("<div class='panel panel-info'>" +
-
-	"<div class='panel-heading'>" + name +
-	"</div>" +
-	"<div class='panel-body'>" + phone +
-	"</div>" + "</div>")
-
-};
